06eca2e9332b10f92c42e3f1a7f912f4754bf63a
CHR9705 <changhoryu97@gmail.com>
2026-07-09 11:22:12 +0900

    update: 신규 db tabel에 따라 config를 변경

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 raw_id = os.getenv("auth_id", "")
 RECIPIENT_IDS = [int(x.strip()) for x in raw_id.split(",")] if raw_id else []
 
-DB_PATH = "your_database.db"
-TABLE_NAME = "your_table"
+DB_PATH = "packets.db"
+TABLE_NAME = "warnings"
 KEY_COLUMN = "id"
